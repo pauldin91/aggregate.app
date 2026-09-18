0739dd6941c7c0e1cf66e8302deb0234b369d5ff
pauldin91 <pauldin91@gmail.com>
2026-09-18 14:57:44 +0300

    fix errors be

--- backend/src/Application/Aggregation.Backend.Application/Features/Air/AirPollutionQueryHandler.cs
+++ backend/src/Application/Aggregation.Backend.Application/Features/Air/AirPollutionQueryHandler.cs
@@ -1,15 +1,16 @@
 using Aggregation.Backend.Application.Interfaces;
+using Aggregation.Backend.Domain.Dtos.External.Air;
 using MediatR;
 
 namespace Aggregation.Backend.Application.Features.Air
 {
-    public record AirPollutionQuery(string Category, string? FilterBy, string? SortBy, bool Asc = true) : IRequest<CityDto>;
+    public record AirPollutionQuery(string Category, string? FilterBy, string? SortBy, bool Asc = true) : IRequest<IList<CityDto>>;
 
     public class AirPollutionQueryHandler(IAirPollutionService airPollutionService) : IRequestHandler<AirPollutionQuery, IList<CityDto>>
     {
         public async Task<IList<CityDto>> Handle(AirPollutionQuery request, CancellationToken cancellationToken)
         {
-            return await airPollutionService.GetAirPollutionDataAsync(request.Category, request.FilterBy, request.SortBy, request.Asc);
+            return await airPollutionService.GetAirPollutionDataAsync(request.Category, cancellationToken);
         }
     }
 }
